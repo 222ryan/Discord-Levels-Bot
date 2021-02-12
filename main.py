277cd061e903d6dda6b5d1918c445bd21c79f23d
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-12 05:40:07 +0000

    Added Configs

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from discord.ext import commands
 import discord
 import levelsys
 
+# Config - More options in levelsys.py!
+prefix = ""  # Sets the bots prefix
+bot_token = ""  # Your discord application token
+
 cogs = [levelsys]
 
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("token here")
+client.run(bot_token)
