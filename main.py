ae72b63d0f02db3b99c07b4f544ff3bb0731d30d
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-13 08:41:02 +0000

    Fixed 'Command Doesn't Exist' Error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 import discord
 from ruamel.yaml import YAML
 import levelsys
@@ -13,11 +14,19 @@ cogs = [levelsys]
 client = commands.Bot(command_prefix=config['Prefix'], intents=discord.Intents.all(), case_insensitive=True)
 client.remove_command('help')
 
+
 @client.event
 async def on_ready():
     configactivity = config['bot_activity']
     activity = discord.Game(name=config['bot_status_text'])
     await client.change_presence(status=configactivity, activity=activity)
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
