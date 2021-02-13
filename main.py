92c6ee7ee522a38532b8bef091b06846090f58cc
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-13 06:47:19 +0000

    Added Status and Activity

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ cogs = [levelsys]
 client = commands.Bot(command_prefix=config['Prefix'], intents=discord.Intents.all())
 client.remove_command('help')
 
+@client.event
+async def on_ready():
+    configactivity = config['bot_activity']
+    activity = discord.Game(name=config['bot_status_text'])
+    await client.change_presence(status=configactivity, activity=activity)
+
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
