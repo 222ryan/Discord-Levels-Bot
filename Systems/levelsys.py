c21ff019c898da1d48f673aef73896c7e4f39593
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-03-27 15:46:25 +0000

    Update levelsys.py

--- Systems/levelsys.py
+++ Systems/levelsys.py
@@ -214,6 +214,8 @@ class levelsys(commands.Cog):
                             value=f"``{prefix}Update <user>`` *Updates any missing database fields for a user when updating to a newer version*")
             embed.add_field(name="Other:",
                             value=f"*You will earn ``{xp}xp`` per message | XP Per Level Is: ``{config['xp_per_level']}xp``*")
+            embed.add_field(name="XP Colour:",
+                            value=f"``{prefix}xpcolour <hex code>`` *Changes the colour of the xp bar if ``image_mode`` is enabled*")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/812895798496591882/825363205853151252/ML_1.png")
             await ctx.channel.send(embed=embed)
 
