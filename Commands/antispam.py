21574460cd96dc763e8848dc4725283269e9f9a9
Kumo's Lab <63556081+KumosLab@users.noreply.github.com>
2021-09-12 23:50:06 +0100

    Bug Fixes

---
+++ Commands/antispam.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+from ruamel.yaml import YAML
+from Systems.levelsys import levelling
+
+# Reads the config file, no need for changing.
+yaml = YAML()
+with open("Configs/config.yml", "r", encoding="utf-8") as file:
+    config = yaml.load(file)
+with open("Configs/spamconfig.yml", "r", encoding="utf-8") as file:
+    spamconfig = yaml.load(file)
+
+
+# Spam system class
+class antispam(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # Reset Command
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    @commands.guild_only()
+    async def antispam(self, ctx, state=None):
+        if spamconfig['antispam_system'] is True:
+            stats = levelling.find_one({"server": ctx.guild.id})
+            if state is None:
+                prefix = config['Prefix']
+                embed2 = discord.Embed(title=f":x: SETUP FAILED",
+                                       description=f"You need to enter a valid state!",
+                                       colour=config['error_embed_colour'])
+                embed2.add_field(name="Example:", value=f"`{prefix}antispam <true|false>`")
+                await ctx.send(embed=embed2)
+            elif state == "true":
+                levelling.update_one({"server": ctx.guild.id}, {"$set": {"Antispam": True}})
+                embed = discord.Embed(title=f":white_check_mark: ANTISPAM ENABLED!", description=f"Anti-Spam now set to: `{state}`",
+                                      colour=config['success_embed_colour'])
+                await ctx.send(embed=embed)
+            elif state == "false":
+                levelling.update_one({"server": ctx.guild.id}, {"$set": {"Antispam": False}})
+                embed = discord.Embed(title=f":white_check_mark: ANTISPAM DISABLED!", description=f"Anti-Spam now set to: `{state}`",
+                                      colour=config['success_embed_colour'])
+                await ctx.send(embed=embed)
+
+
+
+# Sets-up the cog for help
+def setup(client):
+    client.add_cog(antispam(client))
---
+++ Commands/levelchannel.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+from ruamel.yaml import YAML
+from Systems.levelsys import levelling
+
+# Reads the config file, no need for changing.
+yaml = YAML()
+with open("Configs/config.yml", "r", encoding="utf-8") as file:
+    config = yaml.load(file)
+
+
+# Spam system class
+class levelchannel(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # Reset Command
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    @commands.guild_only()
+    async def levelchannel(self, ctx, channel=None):
+        stats = levelling.find_one({"server": ctx.guild.id})
+        if stats is None:
+            newserver = {"server": ctx.guild.id, "level_channel": " "}
+            levelling.insert_one(newserver)
+        else:
+            if channel is None:
+                prefix = config['Prefix']
+                embed2 = discord.Embed(title=f":x: SETUP FAILED",
+                                       description=f"You need to enter a channel name!",
+                                       colour=config['error_embed_colour'])
+                embed2.add_field(name="Example:", value=f"`{prefix}levelchannel <channelname>`\n\n*** Please do not use the # and enter any -'s! ({prefix}levelchannel test-channel)***")
+                await ctx.send(embed=embed2)
+            elif channel:
+                levelling.update_one({"server": ctx.guild.id}, {"$set": {"level_channel": channel}})
+                embed = discord.Embed(title=f":white_check_mark: LEVEL CHANNEL",
+                                      description=f"The new level channel is: `{channel}`",
+                                      colour=config['success_embed_colour'])
+                await ctx.send(embed=embed)
+
+
+# Sets-up the cog for help
+def setup(client):
+    client.add_cog(levelchannel(client))
