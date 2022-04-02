d332d7b2ab35e8c45dd1d621f9d93abe6010e6f4
Kumo's Lab <63556081+KumosLab@users.noreply.github.com>
2022-04-02 15:45:23 +0100

    Update autoupdater.py

--- System/autoupdater.py
+++ System/autoupdater.py
@@ -13,7 +13,7 @@ with open("Configs/config.yml", "r", encoding="utf-8") as file:
     config = yaml.load(file)
 
 
-bot_version = "2.0.1"
+bot_version = "2.0.0"
 
 class autoupdater(commands.Cog):
     def __init__(self, client):
